87212d6ca613a4ad730deac687baa29f7c957d15
Mark Jatana <markjatana@gmail.com>
2013-03-05 08:26:45 +0000

    Factored out javascript to separate file

---
+++ com.mj.jqunit/js/dashboard.js
@@ -0,0 +1,186 @@
+	// test case json state pass value
+	var PASS_KEY = 'passed';
+	
+	// test case json state fail value
+	var FAIL_KEY = 'failed';
+	
+	// style class for passed elements
+	var PASS_CLASS = 'pass';
+	
+	// style class for fail elements
+	var FAIL_CLASS = 'fail';
+
+function generateMarkUp(run){
+		generateRunContainers(run);
+		generateSuites(run, run.id);
+		for(var i = 0; i < run.testSuites.length; i++){
+			generateCases(run.testSuites[i]);
+		}
+		addListeners(run);
+		addPassFailIndicators(run);
+		setFontSize();
+	}
+	
+	function addPassFailIndicators(run){
+
+		var progBar = document.getElementById('progressBar_'+run.id);
+		$(progBar).width((run.completed/run.total)*100+'%');
+		var color = 'red';
+		if(run.failures == 0){
+			color = 'green';
+		}
+		$(progBar).css('background', color);
+		var runMargin = document.getElementById('runMargin_'+run.id);
+		$(runMargin).css('border', 'solid 2px '+color);
+		for(var i = 0; i < run.testSuites.length ; i++){
+			var testSuite = run.testSuites[i];
+			var suiteMarginElement = document.getElementById('suiteMargin_'+testSuite.id);
+			var hasFail = false;
+			for(var x = 0; x < testSuite.testCases.length ; x++){
+				var testCase = testSuite.testCases[x];
+				var caseMarginElement = document.getElementById('caseMargin_'+testCase.id);
+				if(testCase.state == FAIL_KEY){
+				  var hasFail = true;
+			      $(caseMarginElement).addClass(FAIL_CLASS);	  
+				}else if(testCase.state == PASS_KEY){
+				  $(caseMarginElement).addClass(PASS_CLASS);
+				}
+			}
+			if(hasFail){
+			      $(suiteMarginElement).addClass(FAIL_CLASS);
+			}else {
+				  $(suiteMarginElement).addClass(PASS_CLASS);
+			}
+				
+		}
+	}
+	
+	
+	function addListeners(run){
+		for(var i = 0; i < run.testSuites.length; i++){
+			
+			var testSuite = run.testSuites[i];
+			document.getElementById('runMargin_'+run.id).addEventListener("click", createHideListener(['suiteContainer_'+testSuite.id], 'runTwistie_'+run.id), false);
+			
+			var caseContainerIds = [];
+			for(var x=0; x < testSuite.testCases.length; x++){
+				caseContainerIds.push( "caseContainer_"+testSuite.testCases[x].id);
+			}
+			document.getElementById('suiteMargin_'+testSuite.id).addEventListener("click", createHideListener(caseContainerIds, 'suiteTwistie_'+testSuite.id), false);
+		}
+		//add a resize listener to the run container
+		window.addEventListener("resize", resizeThrottler, false);
+	}
+	
+	/**
+	 * Resizes the main test element size
+	 */
+	function setFontSize(){
+		var testContainer = document.getElementById('testContainer');
+		var size = $(testContainer).width();
+		var fontSize = (size/1075)*100;
+		$(testContainer).css('font-size',fontSize+'%');
+		$(document.getElementById('size')).text('Size is '+size);
+	}
+	
+	var resizeTimeout;
+	  function resizeThrottler() {
+	    // ignore resize events as long as an actualResizeHandler execution is in the queue
+	    if ( !resizeTimeout ) {
+	      resizeTimeout = setTimeout(function() {
+	        resizeTimeout = null;
+	        setFontSize();
+	     
+	       // The actualResizeHandler will execute at a rate of 15fps
+	       }, 66);
+	    }
+	  }
+	
+	
+	/**
+	 * Add run containers
+	 */
+	function generateRunContainers(run){
+		$(document.getElementById('testContainer')).prepend('<div id="run_'+ run.id +'" class="runContainer group">'
+				+'<div class="runHeader group">'
+					+'<div id="runMargin_'+ run.id +'" class="runMargin rounded margin group">'
+						+'<div id="runTwistie_'+ run.id +'" class=" twistie runTwistie">-</div>'
+					+'</div>'
+					+'<div class="summary rounded">'
+						+'<ol class="details group">'
+						+ '<li>'+run.name+': '+run.completed+'/'+ run.total +'</li>'
+						+ '<li>Errors: '+ run.errors +'</li>'
+						+ '<li>Failures: '+ run.failures +'</li>'
+						+'</ol>'
+						+'<div id="progressBar_'+ run.id +'"class="progressBar"></div>'
+					+'</div>'
+				+'</div>'
+			+'</div>');
+		
+	}
+	
+	/*
+	 * Add suites
+	 */
+	function generateSuites(run, id){
+			var runContainer = document.getElementById('run_'+id);
+			for(var i = 0; i < run.testSuites.length; i++){
+				var testSuite = run.testSuites[i];
+				$(runContainer).append(
+						'<div id="suiteContainer_'+ testSuite.id +'" class="suiteContainer group">'
+						+	'<div class="suiteHeader group">'
+						+		'<div id="suiteMargin_'+ testSuite.id +'" class="suiteMargin group rounded margin">'
+						+			'<div id="suiteTwistie_'+ testSuite.id +'" class=" twistie">-</div>'
+						+		'</div>'
+						+		'<div class="suiteDetails rounded">'+ testSuite.name +'</div>'
+						+	'</div>'		
+					+	'</div>'		
+			    );
+				
+			}
+	}
+	
+	function generateCases(testSuite){
+		var suiteContainerElement = document.getElementById('suiteContainer_'+testSuite.id);
+		for(var i = 0; i < testSuite.testCases.length; i++){
+			var testCase = testSuite.testCases[i];
+			$(suiteContainerElement).append(
+					'<div id="caseContainer_'+ testCase.id +'" class="caseContainer group">'
+			
+					+	'<div id="caseHeader" class="caseHeader group">'
+					+	'	<div id="caseMargin_'+ testCase.id +'" class="margin caseMargin rounded group odd">'	
+					+			'<div id="caseTwistie_'+ testCase.id +'" class="twistie caseTwistie">+</div>'
+					+	'	</div>'
+					+		'<div id="case_'+ testCase.id +'" class="caseDetails rounded group odd">' + testCase.name +'</div>'
+					+		'</div>'
+			
+					+'</div>');
+		}
+	}
+	
+	
+
+	function createHideListener(ids, twistieId){
+		var hideElements = function(){
+			for(var i=0;i < ids.length;i++){
+				hide(ids[i]);
+			}
+			switchTwistie(twistieId);
+		}
+		return hideElements;
+	}
+
+	function hide(id){
+		$('#'+id).slideToggle('slow');
+	}
+	
+	function switchTwistie(id){
+		var less = "-";
+		var more = "+";
+		var element = document.getElementById(id);
+		if(element.innerHTML == less){
+			element.innerHTML = more;
+		}else{
+			element.innerHTML = less;
+		}
+	}
